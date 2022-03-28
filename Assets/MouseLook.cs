b746dd9f9a0c35e8082647fa5314d9cdccaa4f2b
Dominic Sutter <>
2022-03-28 17:52:32 +0200

    Comments for 3D player

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -7,10 +7,13 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerBody;
 
+    // I think the "f" behind the numbers means "default"
     private float _xRotation = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        // The cursor is locked at the center of the screen and invisible
         Cursor.lockState = CursorLockMode.Locked;
         
     }
@@ -18,9 +21,11 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // the brown "Mouse X" and "Mouse Y" are the positions of the real mouse. "mouseX" and "mouseY" are the variables. We disconnect it from FrameRate
         var mouseX = Input.GetAxis("Mouse X")* mouseSensitivity * Time.deltaTime;
         var mouseY = Input.GetAxis("Mouse Y")* mouseSensitivity * Time.deltaTime;
         
+        // mouseY is for the up and down movement of camera, the "mathClamp" is to fix it so the player can't turn his head vertically 360 degrees
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 70f);
         
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,12 +11,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Define what is horizontal and what is vertical
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Vector3 is movement in a 3D space
+        // local variable "moveDirection" is changed
         Vector3 moveDirection = transform.right * x + transform.forward * z;
 
-        // Vector3 is movement in a 3D space
+        // add speed and disconnect it from frame rate
         controller.Move(moveDirection * speed * Time.deltaTime);
     }
 }
